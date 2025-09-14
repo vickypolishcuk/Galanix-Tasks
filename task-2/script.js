050e98b7fd38986d44645cedc942625fa1383a67
vickypolishcuk <policeviks@gmail.com>
2025-09-14 22:53:21 +0300

    Fix indents

--- task-2/script.js
+++ task-2/script.js
@@ -1,21 +1,19 @@
 function getFormattedDate() {
-  const now = new Date();
+    const now = new Date();
 
-  const day = String(now.getDate()).padStart(2, '0');
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const year = now.getFullYear();
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const year = now.getFullYear();
 
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
 
-  return `${day}.${month}.${year} ${hours}:${minutes}`;
+    return `${day}.${month}.${year} ${hours}:${minutes}`;
 }
 
 function getNumberOfImg() {
     return (document.querySelectorAll('img')).length;
 }
-
-
 
 document.querySelector('.data').textContent = `Date: ${getFormattedDate()}`;
 document.querySelector('.number').textContent = `Number of images: ${getNumberOfImg()}`;
