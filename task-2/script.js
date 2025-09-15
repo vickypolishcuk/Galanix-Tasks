f12b3397b39424dac24c28b25e78c8274bef6831
vickypolishcuk <policeviks@gmail.com>
2025-09-15 11:14:12 +0300

    Modal and comments

--- task-2/script.js
+++ task-2/script.js
@@ -1,19 +1,41 @@
+// Функція отримання й форматування поточної дати
 function getFormattedDate() {
-    const now = new Date();
+  const now = new Date();
 
-    const day = String(now.getDate()).padStart(2, '0');
-    const month = String(now.getMonth() + 1).padStart(2, '0');
-    const year = now.getFullYear();
+  const day = String(now.getDate()).padStart(2, '0');
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const year = now.getFullYear();
 
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
 
-    return `${day}.${month}.${year} ${hours}:${minutes}`;
+  return `${day}.${month}.${year} ${hours}:${minutes}`;
 }
 
+// Функція отримання кількості картинок на сторінці
 function getNumberOfImg() {
-    return (document.querySelectorAll('img')).length;
+  return (document.querySelectorAll('img')).length;
 }
 
+// Виведення інформації на сторінку
 document.querySelector('.data').textContent = `Date: ${getFormattedDate()}`;
 document.querySelector('.number').textContent = `Number of images: ${getNumberOfImg()}`;
+
+// Змінні для відображення модального вікна й картинки
+const galleryImages = document.querySelectorAll('.gallery img');
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modal-img');
+const closeBtn = document.querySelector('.close');
+
+// Обробники для кожної картинки
+galleryImages.forEach(img => {
+  img.addEventListener('click', () => {
+    modal.style.display = 'block'; // відображаємо модалку
+    modalImg.src = img.src; // відображаємо обрану картинку
+  });
+});
+
+// Обробник для закриття картинки
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none'; // не відображаємо модалку
+});
